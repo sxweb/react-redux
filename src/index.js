d965045d7ede3d5280cad67b739834ddb37d2536
sxweb <phpred@yandex.ru>
2021-07-05 09:37:20 +0300

    reducer lesson

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const reducer = (state = 0, action) =>{
+    switch (action.type){
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+}
+let state = reducer(undefined, {});
+state = reducer(state, {type: 'INC'});
+console.log(state);
