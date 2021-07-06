d232fe48240ab84d8a8140c6ff3e4abe9b519b5f
sxweb <phpred@yandex.ru>
2021-07-06 08:32:24 +0300

    project structure refactoring

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import {createStore} from 'redux';
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (payload) => ({type: 'RND', payload});
-const reducer = (state = 0, action) =>{
-    switch (action.type){
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RND':
-            return state + action.payload;
-        default:
-            return state;
-    }
-}
+import reducer from "./reducer";
+import {inc, dec, rnd} from './actions';
+
 
 const store = createStore(reducer);
 document
