f0be9bc42472357df0590d7081d7dabb45d10cc5
sxweb <phpred@yandex.ru>
2021-07-06 08:08:16 +0300

    actions params

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ const reducer = (state = 0, action) =>{
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state + action.payload;
         default:
             return state;
     }
 }
 
 const store = createStore(reducer);
+document
+    .querySelector('#rnd')
+    .addEventListener('click', ()=>{
+        const payload = Math.floor(Math.random() * 10);
+        store.dispatch({
+            type: 'RND',
+            payload
+        });
+    })
 document
     .querySelector('#inc')
     .addEventListener('click', ()=>{
