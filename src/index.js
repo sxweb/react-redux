4f8caf828bf89a6f29c6ec68b554f24d9ecd0ff6
sxweb <phpred@yandex.ru>
2021-07-09 09:09:39 +0300

    implementing react lesson

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Counter from "./counter";
 import {createStore, bindActionCreators} from 'redux';
 import reducer from "./reducer";
 import * as actions from './actions';
@@ -8,25 +11,20 @@ const {dispatch} = store;
 
 const {inc, dec, rnd} =
     bindActionCreators(actions, dispatch);
-document
-    .querySelector('#rnd')
-    .addEventListener('click', ()=>{
-        const payload = Math.floor(Math.random() * 10);
-        rnd(payload);
-    })
-document
-    .querySelector('#inc')
-    .addEventListener('click', ()=>{
-        inc();
-    });
-document
-    .querySelector('#dec')
-    .addEventListener('click', ()=>{
-        dec();
-    });
+
 const update = () =>{
-    document
-        .querySelector('#counter')
-        .textContent = store.getState();
+    ReactDOM.render(<Counter
+            counter={store.getState()}
+            inc = {inc}
+            dec = {dec}
+            rnd = {() => {
+                const payload = Math.floor(Math.random() * 10);
+                rnd(payload);
+            }}
+
+        />,
+        document.getElementById('root')
+    );
 }
+update();
 store.subscribe(update);
