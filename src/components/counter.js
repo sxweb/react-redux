8bd8534050d82eebe17eb6617b16390aefbac5a1
sxweb <phpred@yandex.ru>
2021-07-12 09:22:15 +0300

    simple fixes

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import * as actions from '../actions';
 
 const Counter = ({counter, inc, dec, rnd}) =>{
